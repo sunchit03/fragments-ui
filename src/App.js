644777bacc12d5145126ed9bd376d761597e062e
Sunchit Singh <sunchit333@gmail.com>
2025-02-02 00:05:47 -0500

    using react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,55 +1,89 @@
-import React, { useState, useEffect } from 'react';
-import { signIn, getUser } from './auth';
-import { getUserFragments } from './api';
-import './App.css';
+// App.js
+
+import { useAuth } from 'react-oidc-context';
+import MyNavbar from './components/MyNavbar';
+import { useState, useEffect } from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 function App() {
+  const auth = useAuth();
 
-  const [user, setUser] = useState(null);
+  const [view, setView] = useState('homeView');
 
-  useEffect(() => {
-    async function init() {
-      // See if we're signed in (i.e., we'll have a `user` object)
-      const user = await getUser();
-      if (user) {
-        setUser(user);
-      }
-      // Do an authenticated request to the fragments API server and log the result
-      const userFragments = await getUserFragments(user);
-
-      // TODO: later in the course, we will show all the user's fragments in the HTML...
-    }
-
-    init();
-  }, [])
-
-  // Wire up event handlers to deal with login and logout.
-  const handleLoginClick = () => {
-    // Sign-in via the Amazon Cognito Hosted UI (requires redirects), see:
-    signIn();
+  // eslint-disable-next-line default-case
+  switch (auth.activeNavigator) {
+    case 'signinSilent':
+      return <div>Signing you in...</div>;
+    case 'signoutRedirect':
+      return <div>Signing you out...</div>;
   }
 
+  if (auth.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (auth.error) {
+    return <div>Encountering error... {auth.error.message}</div>;
+  }
+
+  console.log(auth.user);
   return (
-     <div>
+    <div>
       <h1>Fragments UI</h1>
       <section>
-        <nav>
-          {user ? 
-            <button id="login" disabled>Login</button>
-          :
-          <button id="login" onClick={handleLoginClick}>Login</button>
-          }
-        </nav>
+        <MyNavbar auth={auth} setView={setView} />
+        {/* <Navbar expand="lg" className="bg-body-tertiary">
+          <Container>
+            <Navbar.Brand href="#home">Fragments App</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                {auth.isAuthenticated ? (
+                  <>
+                    <Nav.Link onClick={() => setView('homeView')}>Home</Nav.Link>
+                    <Nav.Link onClick={() => setView('fragmentsView')}>View Fragments</Nav.Link>
+                    <Nav.Link onClick={() => setView('createFragmentsView')}>
+                      Create Fragments
+                    </Nav.Link>
+                    <Nav.Link onClick={() => auth.removeUser()}>Log Out</Nav.Link>
+                  </>
+                ) : (
+                  <Nav.Link onClick={() => auth.signinRedirect()}>Log In</Nav.Link>
+                )}
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar> */}
       </section>
-      {user && (
-        <section id="user">
-          <h2>
-            Hello <span className="username">{user.username}</span>!
-          </h2>
-        </section>
-      )}
+      <section id="user">
+        <h2>
+          Hello <span className="username">{auth.user?.profile['cognito:username']}</span>!
+        </h2>
+      </section>
     </div>
   );
+
+  // if (auth.isAuthenticated) {
+  //   return (
+  //     <div>
+  //       <pre> Hello: {auth.user?.profile.email} </pre>
+  //       <pre> ID Token: {auth.user?.id_token} </pre>
+  //       <pre> Access Token: {auth.user?.access_token} </pre>
+  //       <pre> Refresh Token: {auth.user?.refresh_token} </pre>
+
+  //       <button onClick={() => auth.removeUser()}>Sign out</button>
+  //     </div>
+  //   );
+  // }
+
+  // return (
+  //   <div>
+  //     <button onClick={() => auth.signinRedirect()}>Sign in</button>
+  //     <button onClick={() => signOutRedirect()}>Sign out</button>
+  //   </div>
+  // );
 }
 
 export default App;
